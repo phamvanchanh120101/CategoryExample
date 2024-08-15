74eedee810b2df88339011021a71116b9a8ddc5e
Chanh Pham <phamvanchanh120101@gmail.com>
2024-08-15 20:14:20 +0700

    locker item layout

--- app/src/main/java/com/example/categoryexample/ui/component/ModelItem.kt
+++ app/src/main/java/com/example/categoryexample/ui/component/ModelItem.kt
@@ -1,15 +1,15 @@
 package com.example.categoryexample.ui.component
 
 import com.example.categoryexample.R
-import com.example.categoryexample.databinding.ModelItemBinding
+import com.example.categoryexample.databinding.ModelItem1Binding
 import com.example.categoryexample.domain.model.Model
 import com.xwray.groupie.databinding.BindableItem
 
-class ModelItem(private val model: Model) : BindableItem<ModelItemBinding>() {
+class ModelItem(private val model: Model) : BindableItem<ModelItem1Binding>() {
 
-    override fun getLayout() = R.layout.model_item
+    override fun getLayout() = R.layout.model_item1
 
-    override fun bind(viewBinding: ModelItemBinding, position: Int) {
+    override fun bind(viewBinding: ModelItem1Binding, position: Int) {
         viewBinding.model = model
     }
 }
